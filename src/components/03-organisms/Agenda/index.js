dc2851eb1008a40c5d7c58610300b78082eee457
rsafronov <electroma@gmail.com>
2017-04-04 12:14:00 -0400

    Added: stubbed user + "my meeting" flag logic

--- src/components/03-organisms/Agenda/index.js
+++ src/components/03-organisms/Agenda/index.js
@@ -20,6 +20,13 @@ class Agenda extends React.Component {
 
     return (
       <div>
+
+        <div className={styles.user}>
+          <span className={styles.hello}>Hello</span>
+          <span className={styles.name}>
+            { this.props.user }
+          </span>
+        </div>
         <div className={styles.agenda}>
           <TimelineLabelList />
           { rooms.map(room => <RoomTimeline key={room.name} room={room} />) }
@@ -31,22 +38,25 @@ class Agenda extends React.Component {
 }
 
 Agenda.propTypes = {
+  user: PropTypes.string,
   requestRooms: PropTypes.func,
   rooms: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 const mapStateToProps = state => ({
+  user: state.user.name,
   rooms: state.meetings.map(rm => {
     const meetings = rm.meetings.map(m => {
       const startMoment = moment(m.start);
       const endMoment = moment(m.end);
       const duration = endMoment.diff(startMoment, 'minutes') / 60;
+      const isOwnedByUser = m.owner && (state.user.email === m.owner.email);
       return {
         startTime: moment(m.start).format('YYYY-MM-DDTHH:mm:ssZ'),
         duration,
         start: moment(m.start),
         end: moment(m.end),
-        isOwnedByUser: false,
+        isOwnedByUser,
         participants: m.participants,
         owner: m.owner,
         title: m.subject,
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,33 +2,37 @@ const initialState = {
   message: 'Nothing',
   meetings: [],
   rooms: [],
+  user: {
+    email: 'bruce@myews.onmicrosoft.com',
+    name: 'Bruce',
+  },
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'UPDATE_MESSAGE': {
-      return { message: 'That was some Redux!' };
+      return { ...state, message: 'That was some Redux!' };
     }
     case 'USER_FETCH_REQUESTED': {
-      return { message: 'Sending request...' };
+      return { ...state, message: 'Sending request...' };
     }
     case 'USER_FETCH_SUCCEEDED': {
-      return { message: 'Got user info' };
+      return { ...state, message: 'Got user info' };
     }
     case 'USER_FETCH_FAILED': {
-      return { message: 'The request has failed.' };
+      return { ...state, message: 'The request has failed.' };
     }
     case 'ROOMS_REQUESTED': {
       console.log('REQUESTED ROOMS');
-      return { message: 'Getting rooms...' };
+      return { ...state, message: 'Getting rooms...' };
     }
     case 'ROOMS_RECEIVED': {
       console.log('RECEIVED ROOMS');
-      return { rooms: action.rooms };
+      return { ...state, rooms: action.rooms };
     }
     case 'MEETINGS_RECEIVED': {
       console.log('RECEIVED MEETINGS');
-      return { meetings: action.meetings };
+      return { ...state, meetings: action.meetings };
     }
     default: {
       return state;
