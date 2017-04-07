5b3a317da7fe06c9b1e078a7efd4c3ca4ad5eca9
Nicole Tibaldi <ntibaldi@gmail.com>
2017-04-07 09:41:28 -0400

    Fix tests

--- src/utils/calculateTooltipOffset.test.js
+++ src/utils/calculateTooltipOffset.test.js
@@ -2,14 +2,14 @@ import calculateTooltipOffset from './calculateTooltipOffset';
 
 describe('Tooltip offset calculator', () => {
   it('should calculate offset for a 2 hour meeting', () => {
-    expect(calculateTooltipOffset(2)).toBe(-54);
+    expect(calculateTooltipOffset(2)).toBe(-54.5);
   });
 
   it('should calculate offset a 3.5 hour meeting', () => {
-    expect(calculateTooltipOffset(3.5)).toBe(7.5);
+    expect(calculateTooltipOffset(3.5)).toBe(7);
   });
 
   it('should calculate offset for a 15 minute meeting', () => {
-    expect(calculateTooltipOffset(0.25)).toBe(-125.75);
+    expect(calculateTooltipOffset(0.25)).toBe(-126.25);
   });
 });
