2754dbfa0b0437cc14e1660ce9d27ac636e605f4
rsafronov <electroma@gmail.com>
2017-04-14 15:19:18 -0400

    FTFY

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,10 +1,9 @@
-
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import reducer from './reducers';
 
 const configureStore = (initialState) => {
-  const sagaMiddleware = createSagaMiddleware();
+  const sagaMiddleware = createSagaMiddleware.default();
 
   /* eslint-disable no-underscore-dangle */
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
