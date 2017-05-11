c305c90aff4e93c3cee239de1dcfac35ae6b9d23
Peter Monks <walter.monks@wipro.comx>
2017-05-11 16:56:37 -0400

    auto-scrolled the timeline to 8am

--- src/components/02-molecules/RoomTimeline/index.js
+++ src/components/02-molecules/RoomTimeline/index.js
@@ -5,34 +5,39 @@ import Meeting from '../Meeting';
 
 import { HOUR_WIDTH } from '../../../utils/calculateMeetingOffset';
 
-const RoomTimeline = ({ room, meetings, createMeetingRequest }) => {
-  const onTimelineClick = (e) => {
-    const requestedStartTime = e.nativeEvent.offsetX / HOUR_WIDTH;
-    createMeetingRequest(room, requestedStartTime);
-  };
+class RoomTimeline extends React.Component {
+  componentDidMount() {
+    document.getElementById('timelines').scrollLeft = 653;
+  }
+  render() {
+    const onTimelineClick = (e) => {
+      const requestedStartTime = e.nativeEvent.offsetX / HOUR_WIDTH;
+      createMeetingRequest(this.props.room, requestedStartTime);
+    };
 
-  const timelineMeetings = meetings.map((meeting, index) =>
-    <Meeting
-      key={`${room.name}-${index}`}
-      roomTitle={room.name}
-      owner={meeting.owner}
-      isOwnedByUser={meeting.isOwnedByUser}
-      startTime={meeting.start}
-      duration={meeting.duration}
-      title={meeting.title}
-    />
-  );
+    const timelineMeetings = this.props.meetings.map((meeting, index) =>
+      <Meeting
+        key={`${this.props.room.name}-${index}`}
+        roomTitle={this.props.room.name}
+        owner={meeting.owner}
+        isOwnedByUser={meeting.isOwnedByUser}
+        startTime={meeting.start}
+        duration={meeting.duration}
+        title={meeting.title}
+      />
+    );
 
-  return (
-    <div className={styles.room}>
-      <div className={styles.timeline}>
-        <div className={styles.meetings} onClick={onTimelineClick}>
-          { timelineMeetings }
+    return (
+      <div className={styles.room}>
+        <div className={styles.timeline}>
+          <div className={styles.meetings} onClick={onTimelineClick}>
+            { timelineMeetings }
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 RoomTimeline.propTypes = {
   room: PropTypes.shape({
