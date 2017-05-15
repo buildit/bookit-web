dff563244644f8fcdd16567cf75e83efcee02342
Nicole Tibaldi <ntibaldi@gmail.com>
2017-05-15 14:52:07 -0400

    change tooltip anchor logic for long meetings

--- src/components/02-molecules/Meeting/index.js
+++ src/components/02-molecules/Meeting/index.js
@@ -7,6 +7,8 @@ import styles from './styles.scss';
 import calculateWidth from '../../../utils/calculateWidth';
 import { calculateMeetingOffset } from '../../../utils/calculateMeetingOffset';
 
+const TOOLTIP_ANCHOR_RANGE = 250;
+
 class Meeting extends React.Component {
   constructor(props) {
     super(props);
@@ -21,7 +23,16 @@ class Meeting extends React.Component {
   onMove(event) {
     if (this.state.tooltipVisible) {
       const x = event.clientX - event.target.getBoundingClientRect().left;
-      this.setState({ tooltipVisible: true, tooltipOffset: x });
+      let meetingWidth = calculateWidth(this.props.duration);
+      let excessWidth = 0;
+      if (meetingWidth > TOOLTIP_ANCHOR_RANGE) {
+        excessWidth = (meetingWidth - TOOLTIP_ANCHOR_RANGE) / 2;
+      }
+      if (x > TOOLTIP_ANCHOR_RANGE + excessWidth || x < excessWidth) {
+        this.setState({ tooltipVisible: true, tooltipOffset: x < excessWidth ? 0 : TOOLTIP_ANCHOR_RANGE });
+      } else {
+        this.setState({ tooltipVisible: true, tooltipOffset: x - excessWidth });
+      }
     }
   }
 
