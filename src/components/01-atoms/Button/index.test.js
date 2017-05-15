686166eb723fe141153b61c87f2b73df5ddd2edc
Peter Monks <walter.monks@wipro.comx>
2017-05-15 15:25:56 -0400

    fixed linting errors

--- src/components/01-atoms/Button/index.test.js
+++ src/components/01-atoms/Button/index.test.js
@@ -6,26 +6,26 @@ import styles from './styles.scss';
 
 describe('Button Component', () => {
   it('sets the proper class', () => {
-    const wrapper = shallow(<Button type='submit' content='' />);
+    const wrapper = shallow(<Button type="submit" content="" />);
     expect(wrapper.hasClass(styles.button)).toBeTruthy();
   });
   it('sets the correct type', () => {
     const correctType = 'submit';
-    const wrapper = shallow(<Button type={correctType} content='' />);
+    const wrapper = shallow(<Button type={correctType} content="" />);
     expect(wrapper.is(`[type="${correctType}"]`)).toBeTruthy();
   });
   it('sets content correctly', () => {
     const content = 'Foo';
-    const wrapper = shallow(<Button type='submit' content={content} />);
+    const wrapper = shallow(<Button type="submit" content={content} />);
     expect(wrapper.text()).toEqual(content);
   });
   it('can be disabled', () => {
-    const wrapper = shallow(<Button type='submit' content='' disabled={true} />);
-    expect(wrapper.props()['disabled']).toBe(true);
+    const wrapper = shallow(<Button type="submit" content="" disabled />);
+    expect(wrapper.props().disabled).toBe(true);
   });
   it('can set a click function', () => {
     const func = () => (undefined);
-    const wrapper = shallow(<Button type='submit' content='' onClick={func} />);
-    expect(wrapper.props()['onClick']).toBe(func);
+    const wrapper = shallow(<Button type="submit" content="" onClick={func} />);
+    expect(wrapper.props().onClick).toBe(func);
   });
 });
