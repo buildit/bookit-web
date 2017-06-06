9f42396cd8d1380ddde7e051a5ac09a841ceeee2
Zachary Smith <william.smith1@wipro.com>
2017-06-06 15:06:12 -0400

    Put Admin inside newly created layout

--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -1,21 +1,45 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import Header from '../../components/02-molecules/Header';
+import InfoPanel from '../InfoPanel';
 import SearchableUserTable from '../../components/03-organisms/SearchableUserTable';
+import { logout } from '../../actions';
+import styles from './styles.scss';
 
-const Admin = ({ users = [] }) => <SearchableUserTable users={users} />;
+const Admin = ({ user, users = [], onLogoutClick }) => (
+  <div className={styles.admin}>
+    <InfoPanel />
+    <main>
+      <Header user={user} logout={onLogoutClick} />
+      <SearchableUserTable users={users} />
+    </main>
+  </div>
+);
 
 const mapStateToProps = state => ({
   users: state.users,
+  user: state.user,
 });
 
-export default connect(mapStateToProps, null)(Admin);
+const mapDispatchToProps = dispatch => ({
+  onLogoutClick: () => {
+    dispatch(logout());
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Admin);
 
 Admin.propTypes = {
-  users: PropTypes.arrayOf(PropTypes.shape({
+  users: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      location: PropTypes.string.isRequired,
+      team: PropTypes.string.isRequired,
+    }),
+  ),
+  user: PropTypes.shape({
     name: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    team: PropTypes.string.isRequired,
   }),
-),
+  onLogoutClick: PropTypes.func.isRequired,
 };
