089355027acdfcb00af1b7cf6664c92a93891032
Chris Ashurst <christopher.ashurst@wipro.com>
2017-06-12 12:22:56 -0400

    add unit tests + support for unit tests for components

--- src/components/01-atoms/Tooltip/TooltipContent.js
+++ src/components/01-atoms/Tooltip/TooltipContent.js
@@ -11,7 +11,7 @@ const TooltipContent =
       <div className={styles.ownerInfo}>
         <div>
           <strong className={styles.roomTitle}>{ roomName } Room</strong>
-          <p>by { isOwnedByUser ? 'me' : owner.name }</p>
+          <p className="owner-name">by { isOwnedByUser ? 'me' : owner.name }</p>
         </div>
         {isOwnedByUser ? <div onClick={() => onEditClick()} className={styles.edit}>Edit</div> : '' }
       </div>
