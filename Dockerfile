8c0a24dfcb043c682fdeafc7e9731e1da1bba4f1
Chris Ashurst <christopher.ashurst@wipro.com>
2017-07-03 14:58:21 -0400

    docker + travis sitting in a tree

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM node:8-alpine
 
-ENV TERM=xterm
+ENV TERM=xterm FORCE_COLOR=1
+
+RUN yarn config set cache-folder /root/.yarn-cache
 
 ADD package.json yarn.lock /tmp/
-ADD .yarn-cache.tgz /
+ADD .cache/.yarn-cache.tgz /
 
 RUN cd /tmp && yarn
 RUN mkdir -p /opt/app && cd /opt/app && ln -s /tmp/node_modules
@@ -11,3 +13,8 @@ RUN mkdir -p /opt/app && cd /opt/app && ln -s /tmp/node_modules
 COPY . /opt/app
 
 WORKDIR /opt/app
+
+RUN yarn build
+
+# IMPORTANT!
+# https://docs.docker.com/engine/reference/builder/#add
--- scripts/build.sh
+++ scripts/build.sh
@@ -11,27 +11,19 @@ DIR=$(dirname "$(cd -P -- "$(dirname -- "$0")" && pwd -P)")
 
 cd $DIR
 
-if [ ! -f .yarn-cache.tgz ]; then
+if [ ! -f .cache/.yarn-cache.tgz ]; then
   echo "+ build: Init empty .yarn-cache.tgz"
-  tar cvzf .yarn-cache.tgz --files-from /dev/null
+  tar cvzf .cache/.yarn-cache.tgz --files-from /dev/null
 fi
 
 docker build -t bookit-web:latest .
 
-docker run \
-  --rm \
-  --entrypoint cat \
-  bookit-web:latest \
-  /tmp/yarn.lock > /tmp/yarn.lock
+docker run --rm --entrypoint cat bookit-web:latest /tmp/yarn.lock > /tmp/yarn.lock
 
 if ! diff -q yarn.lock /tmp/yarn.lock > /dev/null  2>&1; then
   echo "+ build: Saving Yarn cache"
 
-  docker run \
-    --rm \
-    --entrypoint tar \
-    bookit-web:latest \
-    czf - /usr/local/share/.cache/yarn/v1/ > .yarn-cache.tgz
+  docker run --rm --entrypoint tar bookit-web:latest czf - /root/.yarn-cache/ > .cache/.yarn-cache.tgz
 
   echo "+ build: Saving yarn.lock"
 
