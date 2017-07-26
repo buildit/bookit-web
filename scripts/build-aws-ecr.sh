24a1df37e9ebdd978e449d7232ae6f5ad93d3f05
Greg Pearman <greg.pearman@wipro.com>
2017-07-26 10:46:36 -0600

    Externalize ASW_REGION

--- scripts/build-aws-ecr.sh
+++ scripts/build-aws-ecr.sh
@@ -4,10 +4,7 @@ pip install --user awscli
 PATH=$PATH:/$HOME/.local/bin
 
 REPO_NAME=bookit-aws-ecr
-AWS_REGION=us-east-1
 echo "The AWS region is $AWS_REGION"
-#COMMIT_SHA=fake
-echo "NOTE:  USING FAKE COMMIT_SHA"
 
 EXISTING_REPO=$(aws ecr describe-repositories --region $AWS_REGION --repository-names $REPO_NAME --output text 2>/dev/null)
 REPO_EXISTS=$(echo $?)
@@ -15,7 +12,6 @@ REPO_EXISTS=$(echo $?)
 if [[ $REPO_EXISTS == 0 ]]; then
   echo "Repo already exists ... skipping creation"
   REPO=$(echo $EXISTING_REPO | cut -f 6 -d ' ')
-  echo "Did we extract the repo name?  ${REPO}"
 else
   echo "Creating repo"
   REPO=$(aws ecr create-repository --repository-name $REPO_NAME --region $AWS_REGION --output text | cut -f 6)
@@ -23,6 +19,7 @@ else
 fi
 
 eval $(aws ecr get-login --region $AWS_REGION)
+#use below locally when above fails
 #eval $(aws ecr get-login --region $AWS_REGION | sed 's/-e none//')
 echo "Preparing to tag, repo:  ${REPO},  sha: ${COMMIT_SHA}"
 
