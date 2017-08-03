4c11c10a09e124fda0e89a75e4c30558566f96bf
Chris Ashurst <christopher.ashurst@wipro.com>
2017-08-03 15:25:06 -0400

    revert commented lines in travis support scripts

--- scripts/run-dockerized-functional-tests.sh
+++ scripts/run-dockerized-functional-tests.sh
@@ -1,45 +1,42 @@
 #!/bin/bash
 
-# if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-DOCKERNAME=bookit_functional_tests
+if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
+  DOCKERNAME=bookit_functional_tests
 
-cleanup () {
-  docker-compose -p $DOCKERNAME kill > /dev/null 2>&1
-  docker-compose -p $DOCKERNAME rm -f > /dev/null 2>&1
-}
+  cleanup () {
+    docker-compose -p $DOCKERNAME kill > /dev/null 2>&1
+    docker-compose -p $DOCKERNAME rm -f > /dev/null 2>&1
+  }
 
-awsparam () {
-  echo `aws ssm get-parameters --region us-east-1 --names $1 --with-decryption --output text | cut -f 4`
-}
+  awsparam () {
+    echo `aws ssm get-parameters --region us-east-1 --names $1 --with-decryption --output text | cut -f 4`
+  }
 
-trap 'cleanup ; printf "${RED}Tests Failed For Unexpected Reasons${NC}\n"' HUP INT QUIT PIPE TERM
+  trap 'cleanup ; printf "${RED}Tests Failed For Unexpected Reasons${NC}\n"' HUP INT QUIT PIPE TERM
 
-export BOOKITUSER=$(awsparam BUILDIT_REGULAR_USER_NAME)
-export BOOKITPASSWD=$(awsparam BUILDIT_REGULAR_USER_PASSWORD)
-export CLOUD_CONFIG=$(awsparam CLOUD_CONFIG)
-export BUILDIT_SECRET=$(awsparam BUILDIT_SECRET)
+  export BOOKITUSER=$(awsparam BUILDIT_REGULAR_USER_NAME)
+  export BOOKITPASSWD=$(awsparam BUILDIT_REGULAR_USER_PASSWORD)
+  export CLOUD_CONFIG=$(awsparam CLOUD_CONFIG)
+  export BUILDIT_SECRET=$(awsparam BUILDIT_SECRET)
 
-# cd $(dirname "$(cd -P -- "$(dirname -- "$0")" && pwd -P)")
+  docker-compose -f docker-compose.testcafe.yml -p $DOCKERNAME up -d > /dev/null 2>&1
 
-docker-compose -f docker-compose.testcafe.yml -p $DOCKERNAME up -d > /dev/null 2>&1
+  TEST_EXIT_CODE=`docker wait bookit_testcafe`
+  docker logs bookit_testcafe
 
-TEST_EXIT_CODE=`docker wait bookit_testcafe`
-docker logs bookit_testcafe
+  if [ -z ${TEST_EXIT_CODE+x} ] || [ "$TEST_EXIT_CODE" -ne 0 ] ; then
+    printf "${RED}tests failed${NC} - exitcode: $TEST_EXIT_CODE\n"
+  else
+    printf "${GREEN}tests passed${NC} - exitcode: $TEST_EXIT_CODE\n"
+  fi
 
-if [ -z ${TEST_EXIT_CODE+x} ] || [ "$TEST_EXIT_CODE" -ne 0 ] ; then
-  printf "${RED}tests failed${NC} - exitcode: $TEST_EXIT_CODE\n"
+  cleanup
+
+  exit $TEST_EXIT_CODE
 else
-  printf "${GREEN}tests passed${NC} - exitcode: $TEST_EXIT_CODE\n"
+  printf "${GREEN}skipping functional tests${NC}\n"
 fi
-
-cleanup
-
-# cd -
-
-exit $TEST_EXIT_CODE
-# fi
-
--- scripts/yarn-and-docker-build.sh
+++ scripts/yarn-and-docker-build.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-# if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
-cd $(dirname "$(cd -P -- "$(dirname -- "$0")" && pwd -P)")
+if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
+  cd $(dirname "$(cd -P -- "$(dirname -- "$0")" && pwd -P)")
 
-echo "Building production code..."
-yarn build
+  echo "Building production code..."
+  yarn build
 
-echo "Building docker image for $DOCKER_ORG/$DOCKER_REPO:$COMMIT_SHA..."
-docker build -t $DOCKER_ORG/$DOCKER_REPO:$COMMIT_SHA .
+  echo "Building docker image for $DOCKER_ORG/$DOCKER_REPO:$COMMIT_SHA..."
+  docker build -t $DOCKER_ORG/$DOCKER_REPO:$COMMIT_SHA .
 
-cd -
-# fi
+  cd -
+fi
