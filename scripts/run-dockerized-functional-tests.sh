2bb30e4a923b48505242f5083445c8b8934fc0cd
Greg Pearman <greg.pearman@wipro.com>
2017-08-15 11:30:03 -0600

    - Parameterized container name - Fix bug with command line argument number used

--- scripts/run-dockerized-functional-tests.sh
+++ scripts/run-dockerized-functional-tests.sh
@@ -6,7 +6,8 @@ NC='\033[0m'
 
 if [[ -z "$TRAVIS_PULL_REQUEST" || $TRAVIS_PULL_REQUEST == "false" ]] && [[ -z "$TRAVIS_BRANCH" || $TRAVIS_BRANCH == "89-aws-functional-tests" ]]; then
   DOCKERNAME=bookit_functional_tests
-  DOCKER_COMPOSE_YAML_FILENAME=${0:-docker-compose.testcafe.yml}
+  DOCKER_COMPOSE_YAML_FILENAME=${1:-docker-compose.testcafe.yml}
+  DOCKER_CONTAINER_NAME=${2:-bookit_testcafe}
 
   cleanup () {
     docker-compose -p $DOCKERNAME kill > /dev/null 2>&1
@@ -24,10 +25,10 @@ if [[ -z "$TRAVIS_PULL_REQUEST" || $TRAVIS_PULL_REQUEST == "false" ]] && [[ -z "
   export CLOUD_CONFIG=$(awsparam CLOUD_CONFIG)
   export BUILDIT_SECRET=$(awsparam BUILDIT_SECRET)
 
-  docker-compose -f ${DOCKER_COMPOSE_YAML_FILENAME} -p $DOCKERNAME up -d
+  docker-compose -f ${DOCKER_COMPOSE_YAML_FILENAME} -p ${DOCKERNAME} up -d > /dev/null 2>&1
 
-  TEST_EXIT_CODE=`docker wait bookit_testcafe`
-  docker logs bookit_testcafe
+  TEST_EXIT_CODE=$(docker wait ${DOCKER_CONTAINER_NAME})
+  docker logs ${DOCKER_CONTAINER_NAME}
 
   if [ -z ${TEST_EXIT_CODE+x} ] || [ "$TEST_EXIT_CODE" -ne 0 ] ; then
     printf "${RED}tests failed${NC} - exitcode: $TEST_EXIT_CODE\n"
