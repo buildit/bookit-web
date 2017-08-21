4381c6e54ef12c1ab0ab734ac9d6cdc0a77182de
Chris Ashurst <christopher.ashurst@wipro.com>
2017-08-21 17:57:31 -0400

    wip

--- src/containers/with-meeting.js
+++ src/containers/with-meeting.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import { connect } from 'react-redux'
@@ -12,14 +12,14 @@ import {
 } from '../selectors'
 
 export default () => (WrappedComponent) => {
-  class BaseMeeting extends Component {
-    constructor(props) {
-      super(props)
-    }
-    render() {
-      return <WrappedComponent {...this.props} />
-    }
-  }
+  // class BaseMeeting extends Component {
+  //   constructor(props) {
+  //     super(props)
+  //   }
+  //   render() {
+  //     return <WrappedComponent {...this.props} />
+  //   }
+  // }
 
   const mapStateToProps = createPropsSelector({
     title: getMeetingTitle,
@@ -28,7 +28,7 @@ export default () => (WrappedComponent) => {
     room: getMeetingRoomName,
   })
 
-  const WithMeeting = connect(mapStateToProps)(BaseMeeting)
+  const WithMeeting = connect(mapStateToProps)(WrappedComponent)
 
   WithMeeting.propTypes = {
     meetingId: PropTypes.string.isRequired,
