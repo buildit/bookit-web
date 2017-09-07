575c38f4c0987f484faf3c8541277b467494ccda
Greg Pearman <greg.pearman@wipro.com>
2017-09-07 11:58:23 -0600

    - Move yarn build to ci/cd pipeline

--- scripts/run-ci-cd.sh
+++ scripts/run-ci-cd.sh
@@ -2,6 +2,7 @@
 
 ./scripts/install-awscli.sh \
   && ./scripts/get-deployment-scripts.sh \
+  && yarn build \
   && ./scripts/push-to-ecr.sh \
   && ./scripts/ecs-deploy.sh -c ${INT_ECS_CLUSTER} -n ${INT_ECS_SERVICE} -i 006393696278.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECS_REPO}:${COMMIT_SHA} \
   && ./scripts/run-dockerized-functional-tests.sh docker-compose.testcafe.aws.yml bookit_testcafe_aws \
