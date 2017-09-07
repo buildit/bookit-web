c3fda7a7643c7573a6fd29d875000357b0ad231d
Greg Pearman <greg.pearman@wipro.com>
2017-09-07 13:02:48 -0600

    - Remove yarn install to ci/cd pipeline

--- scripts/run-ci-cd.sh
+++ scripts/run-ci-cd.sh
@@ -2,7 +2,6 @@
 
 ./scripts/install-awscli.sh \
   && ./scripts/get-deployment-scripts.sh \
-  && yarn install \
   && yarn build \
   && ./scripts/push-to-ecr.sh \
   && ./scripts/ecs-deploy.sh -c ${INT_ECS_CLUSTER} -n ${INT_ECS_SERVICE} -i 006393696278.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECS_REPO}:${COMMIT_SHA} \
