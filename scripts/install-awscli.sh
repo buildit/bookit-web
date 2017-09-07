70ccfbf00368e6076863256bcd0ffdf01d6bbfb2
Greg Pearman <greg.pearman@wipro.com>
2017-09-07 11:42:41 -0600

    - Consolidate CI tasks into one master script

---
+++ scripts/install-awscli.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+pip install --user awscli
+PATH=$PATH:/$HOME/.local/bin
--- scripts/run-ci-cd.sh
+++ scripts/run-ci-cd.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-./scripts/ecs-deploy.sh -c ${INT_ECS_CLUSTER} -n ${INT_ECS_SERVICE} -i 006393696278.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECS_REPO}:${COMMIT_SHA} \
+./scripts/install-awscli.sh \
+  && ./scripts/get-deployment-scripts.sh \
+  && ./scripts/push-to-ecr.sh \
+  && ./scripts/ecs-deploy.sh -c ${INT_ECS_CLUSTER} -n ${INT_ECS_SERVICE} -i 006393696278.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECS_REPO}:${COMMIT_SHA} \
   && ./scripts/run-dockerized-functional-tests.sh docker-compose.testcafe.aws.yml bookit_testcafe_aws \
   && ./scripts/ecs-deploy.sh -c ${STG_ECS_CLUSTER} -n ${STG_ECS_SERVICE} -i 006393696278.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECS_REPO}:${COMMIT_SHA}
