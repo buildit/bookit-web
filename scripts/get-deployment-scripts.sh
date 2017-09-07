edefc93c26c933aa65cec53b193c266187b5f04b
Greg Pearman <greg.pearman@wipro.com>
2017-09-07 10:34:24 -0600

    - Install awscli inside get-deployment-scripts instead of travis - Check integration deployment success before running integration tests

--- scripts/get-deployment-scripts.sh
+++ scripts/get-deployment-scripts.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+pip install --user awscli
+PATH=$PATH:/$HOME/.local/bin
+
 aws s3 cp s3://${S3_BUCKET_BASE}.${AWS_DEFAULT_REGION}.build/app-deployment ./scripts/ --recursive
 chmod +x ./scripts/*.sh
--- scripts/run-ci-cd.sh
+++ scripts/run-ci-cd.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-./scripts/ecs-deploy.sh -c ${INT_ECS_CLUSTER} -n ${INT_ECS_SERVICE} -i 006393696278.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECS_REPO}:${COMMIT_SHA}
-./scripts/run-dockerized-functional-tests.sh docker-compose.testcafe.aws.yml bookit_testcafe_aws
-TEST_EXIT=$?
-if [ $TEST_EXIT -eq 0 ]; then ./scripts/ecs-deploy.sh -c ${STG_ECS_CLUSTER} -n ${STG_ECS_SERVICE} -i 006393696278.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECS_REPO}:${COMMIT_SHA}; fi
+./scripts/ecs-deploy.sh -c ${INT_ECS_CLUSTER} -n ${INT_ECS_SERVICE} -i 006393696278.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECS_REPO}:${COMMIT_SHA} \
+  && ./scripts/run-dockerized-functional-tests.sh docker-compose.testcafe.aws.yml bookit_testcafe_aws \
+  && ./scripts/ecs-deploy.sh -c ${STG_ECS_CLUSTER} -n ${STG_ECS_SERVICE} -i 006393696278.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECS_REPO}:${COMMIT_SHA}
