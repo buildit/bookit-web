0cb4e6fed1477ae5c97c824852081a8f82d53244
Greg Pearman <greg.pearman@wipro.com>
2017-09-07 11:01:41 -0600

    - Remove redundant awscli install which is already handled in travis

--- scripts/get-deployment-scripts.sh
+++ scripts/get-deployment-scripts.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-pip install --user awscli
-PATH=$PATH:/$HOME/.local/bin
-
 aws s3 cp s3://${S3_BUCKET_BASE}.${AWS_DEFAULT_REGION}.build/app-deployment ./scripts/ --recursive
 chmod +x ./scripts/*.sh
