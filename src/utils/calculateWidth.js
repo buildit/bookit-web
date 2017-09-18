33f7b5a6677806575c2b9b7ef2c7fcd3878536f4
Chris Ashurst <christopher.ashurst@wipro.com>
2017-09-18 17:01:30 -0400

    [ci skip] remove more legacy code and fix project dependencies

--- src/utils/calculateWidth.js
+++
@@ -1,7 +0,0 @@
-const WIDTH = 82
-
-// we are subtracting 3 from the final calculated width so that the
-// meeting does not overlap the hour markers on the timeline
-const calculateWidth = duration => (WIDTH * duration) - 3
-
-export default calculateWidth
